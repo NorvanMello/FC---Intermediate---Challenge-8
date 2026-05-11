bcc9c3dcf26ec0130b45bda309d2d2c5f06fbf9b
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-05-11 14:47:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // Global
-const body = document.querySelector("body")
+const body = document.body;
 
 // Header
 const fontSelectorContainer = document.querySelector(".font-selector-container");
+const fontPopup = document.querySelector(".popup")
 const themeLogo = document.querySelector("#theme-logo");
-
-// fontSelectorContainer.addEventListener("click", buttonPressed)
 
 function applyThemeStyles() {
     body.classList.toggle("light-theme")
 }
 
 themeLogo.addEventListener("change", applyThemeStyles)
+
+fontSelectorContainer.addEventListener("click", () => {
+    fontPopup.classList.toggle("active")
+
+    const isOpen = fontPopup.classList.contains("active")
+
+    fontSelectorContainer.setAttribute("aria-expanded", String(isOpen))
+    fontPopup.setAttribute("aria-hidden", String(!isOpen));
+})
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "Escape") {
+        fontPopup.classList.remove("active")
+
+        fontSelectorContainer.setAttribute("aria-expanded", "false")
+        fontPopup.setAttribute("aria-hidden", "true");
+    }
+})
