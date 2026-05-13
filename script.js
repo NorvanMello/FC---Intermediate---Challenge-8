c41c2988d995a156d919c5ecc90520c4713dc123
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-05-13 08:22:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ const btnMono = document.querySelector(".mono")
 
 const fontText = document.querySelector(".font-text")
 
+//Search Container
+const form = document.querySelector(".search-container")
+const searchInput = document.querySelector("#search-input")
+
+// Pronunciation Container
+const wordText = document.querySelector(".word")
+const pronunciation = document.querySelector(".pronunciation")
+const pronunciationPlay = document.querySelector(".pronunciation-button")
+
+// Functions
 function applyThemeStyles() {
     body.classList.toggle("light-theme")
 }
@@ -23,8 +33,21 @@ function updatePopupState(isOpen) {
 
 function closePopup() {
     fontPopup.classList.remove("active")
+    updatePopupState(false);
 }
 
+function changeFont({text, textElement, classToAdd, classToRemove, element}) {
+    classToRemove.forEach(c => {
+        element.classList.remove(c);
+    });
+
+    textElement.textContent = text;
+    element.classList.add(classToAdd);
+
+    closePopup()
+}
+
+// Code
 themeLogo.addEventListener("change", applyThemeStyles)
 
 // Button - Select Font
@@ -38,40 +61,90 @@ fontSelectorContainer.addEventListener("click", () => {
 
 document.addEventListener("keydown", (e) => {
     if(e.key === "Escape") {
-        fontPopup.classList.remove("active")
-
-        updatePopupState(false);
+        closePopup();
     }
 })
 
 // Button Popup - Select Font
 btnSansSerif.addEventListener("click", () => {
-    body.classList.remove("serif")
-    body.classList.remove("mono")
-
-    fontText.textContent = "Sans Serif"
-    body.classList.add("sans-serif")
-
-    closePopup()
-    
+    changeFont({
+        text: "Sans Serif", 
+        textElement: fontText,
+        classToAdd: "sans-serif", 
+        classToRemove: ["serif", "mono"], 
+        element: body
+    })
 })
 
 btnSerif.addEventListener("click", () => {
-    body.classList.remove("sans-serif")
-    body.classList.remove("mono")
-
-    fontText.textContent = "Serif"
-    body.classList.add("serif")
-
-    closePopup()
+    changeFont({
+        text: "Serif", 
+        textElement: fontText,
+        classToAdd: "serif", 
+        classToRemove: ["sans-serif", "mono"], 
+        element: body
+    })
 })
 
 btnMono.addEventListener("click", () => {
-    body.classList.remove("sans-serif")
-    body.classList.remove("serif")
-
-    fontText.textContent = "Mono"
-    body.classList.add("mono")
-
-    closePopup()
+    changeFont({
+        text: "Mono",
+        textElement: fontText,
+        classToAdd: "mono", 
+        classToRemove: ["serif", "sans-serif"], 
+        element: body
+    })
 })
+
+// API
+async function getWord(word) {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    if(!response.ok) {
+        throw new Error("");
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+function renderPronunciationSection(wordArray) {
+    wordText.textContent = `${wordArray.word}`
+    pronunciation.textContent = `${wordArray.phonetic}`
+}
+
+function playAudio() {
+    const audioUrl = pronunciationPlay.dataset.audio;
+
+    if (!audioUrl) return;
+
+    const audio = new Audio(audioUrl);
+    audio.play()
+}
+
+function getAudioUrl(phonetics) {
+    for(phonetic of phonetics) {
+        if(phonetic.audio) {
+            return phonetic.audio
+        }
+    }
+
+    return "";
+}
+
+// Logic
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const wordArray = await getWord(searchInput.value)
+    const wordData = wordArray[0]
+
+    const audioUrl = getAudioUrl(wordData.phonetics)
+
+    pronunciationPlay.dataset.audio = audioUrl;
+
+    renderPronunciationSection(wordData)
+})
+
+pronunciationPlay.addEventListener("click", playAudio)
+
